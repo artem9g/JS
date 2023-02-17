e72bad947f4dfa4dad3c7b04bde74c2eaee8b3b7
artem9g <98230920+artem9g@users.noreply.github.com>
2023-02-17 19:43:15 +0200

    17.02

---
+++ Lesson 3/js/script.js
@@ -0,0 +1,17 @@
+// Задача 0. Обчислити значення виразів 1) S1=a+12+b; 2) S2=a+b2a;  3) S3=3(a+b)c;  4) S4=sin⁡(a-b).
+// 1) S1=a+12+b;
+
+{
+    let a = 10
+    let b = 20
+    let S1 = a + 12 + b;
+    document.write(`Задача 0.`)
+    document.write(`1) S1 = ${S1}`)
+}
+// 2) S2=a+b2a;
+{
+    let a = 10
+    let b = 20
+    let S2 = a + (b * 2 * a);
+    document.write(`1) S2 = ${S2}`)
+}
